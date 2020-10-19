6db0381b37c43bd09a51dbae2c9076c08051a743
ruhitbaidya <you@domain.com>
2020-10-19 23:40:21 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false,
+                loop:true
+            },
+            1000:{
+                items:2,
+                nav:false,
+                loop:true
+            },
+            1200:{
+                items:4,
+                nav:false,
+                loop:true
+            },
+            2000:{
+                items:4,
+                nav:false,
+                loop:true
+            }
+        }
+    })
+  });
+  var mixer = mixitup('.grid-protfolio-container');
